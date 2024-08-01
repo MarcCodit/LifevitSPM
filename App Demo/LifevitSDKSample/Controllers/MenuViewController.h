f07ac8ea4e0f160d702cdcdaaa1dfd77fbecb61d
Marc Aparicio <marc.aa@coditramuntana.com>
2024-08-01 10:16:00 +0200

    Added - LifevitSDKSample project

---
+++ App Demo/LifevitSDKSample/Controllers/MenuViewController.h
@@ -0,0 +1,14 @@
+//
+//  MenuViewController.h
+//  LifevitSDKSample
+//
+//  Created by iNMovens Solutions on 11/5/18.
+//  Copyright © 2018 Lifevit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@import LifevitSDK;
+
+@interface MenuViewController : UIViewController<UITableViewDelegate, UITableViewDataSource, LifevitSDKManagerDelegate>
+
+@end
